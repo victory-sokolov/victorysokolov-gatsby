e5821ebe96e481e82f35c6b2c8ab592ed7e701a6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-03-13 21:45:44 +0200

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import SocialMedia from './SocialMedias';
+import { ContainerStyle } from '../assets/styles/GlobalStyles';
 
-const Footer = () => {
+const FooterStyles = styled.footer`
+    display: flex;
+    /* height: 160px; */
+    background-color: #012C35;
+    color: #fff;
+    text-align: center;
+
+    .footer-content {
+        h6 {
+            text-transform: uppercase;
+            letter-spacing: 1.2px;
+        }
+        p {
+            font-size: 14px;
+        }
+        ul {
+            position: relative;
+            right: 12px;
+        }
+    }
+`;
+
+export default function Footer() {
 
     return (
-        <footer>
-            &copy 2020 Blog
-        </footer>
+
+            <FooterStyles>
+                <ContainerStyle>
+                <div className="footer-content">
+                    <h6>ViktorSokolov.dev</h6>
+                     <SocialMedia color="white"/>
+                    <p>&copy; 2020. All Rights Reserved</p>
+                </div>
+                </ContainerStyle>
+            </FooterStyles>
+
     )
 }
-
-export default Footer;
