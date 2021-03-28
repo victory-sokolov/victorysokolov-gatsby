d7f89ac9657fb57de67f7249dbe3e323efca2677
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-03-28 22:59:08 +0300

    site configs

---
+++ config/site.ts
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'Viktor Sokolov | Personal blog',
+    siteUrl: process.env.ROOT_URL,
+    lang: 'en',
+    pathPrefix: '/',
+    siteLogo: 'images/logo.png',
+    description: 'Tutorials about TypeScript, React, Angular, Python and business bootstraping.',
+    author: 'Viktor Sokolov',
+
+    userTwitter: '@victorysokolov', // Twitter Username
+    ogLanguage: 'en_US',
+
+    // Manifest and Progress color
+    themeColor: '#4147DC',
+    backgroundColor: '#231C42',
+
+    // Social medias
+    twitter: 'https://twitter.com/victorysokolov',
+    twitterHandle: '@victorysokolov',
+    github: 'https://github.com/victory-sokolov',
+    linkedin: 'https://www.linkedin.com/in/sokolov-viktor',
+    youtube: '',
+    rss: '',
+}
