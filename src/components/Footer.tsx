2baa2de20333bf1ff81e2d9707ac1d2bb2df4a2c
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-23 20:45:29 +0300

    update Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import SocialMedia from './SocialMedias';
-import { ContainerStyle } from '../assets/styles/GlobalStyles';
+import React from "react"
+import styled from "styled-components"
+import { ContainerStyle } from "../assets/styles/GlobalStyles"
+import SocialMedia from "./SocialMedias"
 
 const FooterStyles = styled.footer`
-    display: flex;
-    background: var(--primary-gradient);
-    color: #fff;
-    text-align: center;
+  display: flex;
+  background: var(--primary-gradient);
+  color: #fff;
+  text-align: center;
+  font-size: 2rem;
 
-    .footer-content {
-        h6 {
-            text-transform: uppercase;
-            letter-spacing: 1.2px;
-        }
-        p {
-            font-size: 1.2rem;
-        }
-        ul {
-            position: relative;
-            right: 12px;
-        }
+  .footer-content {
+    a {
+      color: #fff;
+      text-transform: uppercase;
+      letter-spacing: 1.4px;
     }
-`;
+    p {
+      font-size: 1.3rem;
+    }
+    ul {
+      position: relative;
+      right: 20px;
+    }
+  }
+`
 
-export default function Footer() {
-
-    return (
-        <FooterStyles>
-            <ContainerStyle>
-            <div className="footer-content">
-                <h6>ViktorSokolov.dev</h6>
-                    <SocialMedia color="white"/>
-                <p>&copy; 2020. All Rights Reserved</p>
-            </div>
-            </ContainerStyle>
-        </FooterStyles>
-    )
+export function Footer() {
+  return (
+    <FooterStyles>
+      <ContainerStyle>
+        <div className="footer-content">
+          <a href="/">
+            <h6>ViktorSokolov.dev</h6>
+          </a>
+          <SocialMedia color="white" />
+          <p>&copy; {new Date().getFullYear()}. All Rights Reserved</p>
+        </div>
+      </ContainerStyle>
+    </FooterStyles>
+  )
 }
