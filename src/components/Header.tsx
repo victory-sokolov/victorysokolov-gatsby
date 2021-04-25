39688fe353fffeaf01f5f2f7cd10a70ce10a71a8
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-25 18:13:05 +0300

    display Hero component only on main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import styled from 'styled-components';
-import "normalize.css"
-import Nav from './Nav';
-import { ContainerStyle } from '../assets/styles/GlobalStyles';
-import Hero from './Hero';
+import { globalHistory } from '@reach/router';
+import { Link } from "gatsby";
+import "normalize.css";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { ContainerStyle } from "../assets/styles/GlobalStyles";
+import Hero from "./Hero";
+import Nav from "./Nav";
 
 const HeaderStyles = styled.header`
   background: var(--primary-gradient);
-  min-height: 260px;
+  min-height: 250px;
   position: relative;
 
   .heading-content {
@@ -21,7 +22,7 @@ const HeaderStyles = styled.header`
   .wave-bottom {
     position: absolute;
     left: 0;
-    bottom:0;
+    bottom: 0;
     width: 100%;
   }
 
@@ -30,7 +31,7 @@ const HeaderStyles = styled.header`
   }
 
   .wave {
-    bottom:-6px;
+    bottom: -6px;
     fill: #fff;
   }
 
@@ -48,7 +49,6 @@ const HeaderStyles = styled.header`
       &:hover > span:last-child {
         animation: moveTextLeft 1s;
       }
-
     }
 
     span:last-child {
@@ -58,30 +58,54 @@ const HeaderStyles = styled.header`
   }
 
   .color-mode {
-    z-index:10;
+    z-index: 10;
     fill: white;
   }
-`;
 
-export default function Header() {
+  @media ${props => props.theme.breakpoints.mobile} {
+    min-height: 185px;
+  }
+`
 
-    return (
-      <HeaderStyles>
-        <ContainerStyle>
-          <div className="heading-content">
-            <svg preserveAspectRatio="none" className="wave-bottom" height="74" viewBox="0 0 1440 74"><path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path></svg>
-            <svg preserveAspectRatio="none" className="wave" height="74" viewBox="0 0 1440 74"><path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path></svg>
-            <h1>
-              <Link to="/">
-                Viktor Sokolov
-              </Link>
-            </h1>
-            <Nav />
-          </div>
-          <Hero />
-        </ContainerStyle>
-      </HeaderStyles>
+export function Header() {
+  const [isRootUrl, setIsRootUrl] = useState(((location.pathname === '/') ? true : false));
 
-    )
+  useEffect(() => {
+    return globalHistory.listen(({ action }) => {
+      if (action === 'PUSH') {
+        setIsRootUrl((location.pathname === '/') ? true : false);
+      }
+    })
+  }, [])
+
+
+  return (
+    <HeaderStyles>
+      <ContainerStyle>
+        <div className="heading-content">
+          <svg
+            preserveAspectRatio="none"
+            className="wave-bottom"
+            height="74"
+            viewBox="0 0 1440 74"
+          >
+            <path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
+          </svg>
+          <svg
+            preserveAspectRatio="none"
+            className="wave"
+            height="74"
+            viewBox="0 0 1440 74"
+          >
+            <path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
+          </svg>
+          <h1>
+            <Link to="/">Viktor Sokolov</Link>
+          </h1>
+          <Nav />
+        </div>
+        {isRootUrl && <Hero /> }
+      </ContainerStyle>
+    </HeaderStyles>
+  )
 }
-
