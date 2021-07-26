7ea3b80f15a66c34f747ff51f37de35f9b6f0b1e
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-07-26 19:08:58 +0300

    update plugins and site settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-const config =  require('./config/site.ts');
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -6,9 +5,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: config.title,
-    author: config.author,
-    description: config.description,
+    title: "Viktor Sokolov - Personal blog",
+    author: "Viktor Sokolov",
+    description: "Blog about Software Enginnering",
     siteUrl: process.env.ROOT_URL,
     keywords: ["Software Engineer", "Sass business", "Web development"],
   },
@@ -18,10 +17,9 @@ module.exports = {
       resolve: `gatsby-plugin-react-helmet`,
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-styled-components`,
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`,
+        displayName: false,
       },
     },
     {
@@ -34,8 +32,39 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
+        path: `${__dirname}/src`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/templates/markdown-page.tsx"),
+        },
+        extensions: [`.md`, `.mdx`, `.markdown`],
+        gatsbyRemarkPlugins: [
+          { resolve: "gatsby-remark-copy-linked-files" },
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
     },
     {
@@ -48,20 +77,8 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.md`, `.mdx`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1200,
-            },
-          },
-        ],
-      },
-    },
+    `gatsby-plugin-image`,
+    `gatsby-remark-reading-time`,
     {
       resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
