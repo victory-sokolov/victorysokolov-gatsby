d846ed508e2b7aad9bd858f2081267ea07328dc6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-07-26 19:09:38 +0300

    site configs

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,28 @@
-module.exports = {
+interface SiteConfig {
+    title: string;
+    siteUrl: string | undefined;
+    lang: string;
+    pathPrefix: string;
+    /* path to logo image */
+    siteLogo: string;
+    description: string;
+    author: string;
+
+    /* Twitter og */
+    userTwitter: string;
+    ogLanguage: string;
+
+    /* Social medias links */
+    twitter: string;
+    twitterHandle: string;
+    github: string;
+    linkedin: string;
+    instagram: string;
+    youtube: string;
+    rss: string;
+}
+
+const site: SiteConfig = {
     title: 'Viktor Sokolov | Personal blog',
     siteUrl: process.env.ROOT_URL,
     lang: 'en',
@@ -11,8 +35,8 @@ module.exports = {
     ogLanguage: 'en_US',
 
     // Manifest and Progress color
-    themeColor: '#4147DC',
-    backgroundColor: '#231C42',
+    // themeColor: '#4147DC',
+    // backgroundColor: '#231C42',
 
     // Social medias
     twitter: 'https://twitter.com/victorysokolov',
@@ -21,5 +45,7 @@ module.exports = {
     linkedin: 'https://www.linkedin.com/in/sokolov-viktor',
     instagram: '',
     youtube: '',
-    rss: '',
+    rss: ''
 }
+
+export default site;
