26a58e84ee9b9fd496517e6923f7b2bd18be2ba7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-08-30 18:42:09 +0300

    update styles

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,32 +1,35 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import styled from 'styled-components';
 
-const CategoriesStyle = styled.a`
+const CategorieWrapperStyle = styled.div`
+  span {
     background: var(--primary-color);
     border-radius: 10px;
     box-shadow: var(--light-shadow);
     text-align: center;
     letter-spacing: 1.2px;
-    cursor: pointer;
-    padding: 5px;
+    padding: 7px;
     color: white;
     font-size: 1.1rem;
-    margin-left:15px;
-
-    &:hover {
-        filter: brightness(120%);
-    }
-`;
+  }
+`
+const CategorieStyle = styled.div`
+  display: inline-block;
+  padding-bottom: 5px;
+  &:not(:first-child) {
+    margin-left: 10px;
+  }
+`
 
 export default function Categories({categories}: {categories: Array<string>}) {
-
     return (
-        categories.map((category: string) => (
-            <span>
-                <CategoriesStyle>
-                    {category}
-                </CategoriesStyle>
-            </span>
-        ))
+      <CategorieWrapperStyle>
+        {categories.map((category: string, index: number) => (
+          <CategorieStyle key={index}>
+            <span>{category}</span>
+          </CategorieStyle>
+        ))}
+      </CategorieWrapperStyle>
     )
 }
--- src/components/PostMetaInfo.tsx
+++ src/components/PostMetaInfo.tsx
@@ -1,5 +1,11 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+
+type PostMetaInfo = {
+  date: string,
+  readTime: string,
+  children?: any
+}
 
 export const MetaPostWrapper = styled.div`
   display: flex;
@@ -8,23 +14,25 @@ export const MetaPostWrapper = styled.div`
   font-weight: 100;
   font-size: 1.5rem;
   color: #5b7a81;
+  padding-bottom: 10px;
 
   small:not(:first-child) {
     margin-left: 1.5rem;
   }
 `
 
-export const PostMetaInfo = ({ date, readTime }) => {
+export const PostMetaInfo = ({ date, readTime, children }: PostMetaInfo) => {
   return (
     <MetaPostWrapper>
       <small>
         <time dateTime={date} itemProp="datePublished">
-          {date}
+          Date published:  {date}
         </time>
       </small>
       <small>
         <span title="Estimated read time">{readTime}</span>
       </small>
+      {children}
     </MetaPostWrapper>
   )
 }
