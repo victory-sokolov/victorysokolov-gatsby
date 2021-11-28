1c2f4896ab2f959faeb9b19d650307db8b658053
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-11-28 11:32:10 +0200

    add google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -147,6 +147,27 @@ export default {
           }
         ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          process.env.GOOGLE_TRACKING_ID,
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          respectDNT: true,
+        }
+      }
     }
   ]
 }
